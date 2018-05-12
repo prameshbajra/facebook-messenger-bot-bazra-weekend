7eb3f59bb8978828565ef5777a9f5682690d1b3c
prameshbajra <pe.messh@gmail.com>
2018-05-12 14:11:46 +0530

    Making bots work ...

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
-            if (text === 'SHOW mE'.ignoreCase()) {
+            if (text === 'show') {
                 sendGenericMessage(sender)
                 continue
             }
